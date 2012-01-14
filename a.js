faa4615ba64f67d11e92675a78d032e478846dd1
johnjbarton <johnjbarton@johnjbarton.com>
2012-01-14 11:41:32 -0800

    Somehow these files ended up too high when trying to get gh-pages to work

--- a.js
+++
@@ -1,8 +0,0 @@
-/*globals window */
-
-function writeOnPage() {
-  var elt = window.document.querySelector('#writeHere');
-  elt.innerHTML = "Kilroy was here";
-}
-
-window.addEventListener('load', writeOnPage, false);
