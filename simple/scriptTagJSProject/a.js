014ac4c179b59325172ea1d972449d6018c3531b
johnjbarton <johnjbarton@johnjbarton.com>
2012-02-01 15:35:38 -0800

    write to the console so we see something happen

--- simple/scriptTagJSProject/a.js
+++ simple/scriptTagJSProject/a.js
@@ -1,8 +1,9 @@
-/*globals window */
+/*globals window console*/
 
 function writeOnPage() {
   var elt = window.document.querySelector('#writeHere');
   elt.innerHTML = "Kilroy was here";
+  console.log("#writeHere <- "+elt.innerHTML);
 }
 
 window.addEventListener('load', writeOnPage, false);
